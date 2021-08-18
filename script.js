c730c437015af0050f3cd962ccdd8c60fb7ff79b
Vasant Tholappa <vasant.tholappa@gmail.com>
2021-08-18 15:33:18 +0530

    Done with functionalities

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let i = -1; // position of last book in myLibrary[]
 
 function Book(title,author,pages,read) {
     // the constructor...
@@ -7,21 +8,34 @@ function Book(title,author,pages,read) {
     this.pages = pages;
     this.read = read;
 
+    // console.log("hi2");
+
     addBookToLibrary(this);
 }
 
+
+
 function addBookToLibrary(b) {
     // takes user input and stores new book object into array
-    let n = myLibrary.length;
-    myLibrary[n] = b;
+    i++;
+    
+    myLibrary[i] = b;
+    
+    // console.log("hi3");
+
+    displayBooks();
 }
 
 
+
+
 const display = document.querySelector(".display");
+
 function displayBooks() {
 
-    for(let i=0;i<myLibrary.length;i++)
-    {
+    // console.log("hi4");
+
+
         let x = myLibrary[i];
 
         const bDiv = document.createElement("div");
@@ -33,7 +47,7 @@ function displayBooks() {
 
         const bTitle = document.createElement("div");
         bTitle.setAttribute("id", "title" + i);
-        bTitle.textContent = "Book" + Number(i+1) + ":"  + x.title;
+        bTitle.textContent = "Title: "  + x.title;
         bTitle.style.cssText = 'padding: 3px; font-size: 30px';
 
         const bAuthor = document.createElement("div");
@@ -52,6 +66,7 @@ function displayBooks() {
         {
             bRead.textContent = "Read";
             bRead.style.cssText = 'padding: 3px; font-size: 25px; color: green';
+            // console.log("hi5");
 
         }
         else if(x.read == "N" || x.read == "n")
@@ -76,6 +91,7 @@ function displayBooks() {
         bDiv.appendChild(bRemove);
 
 
+        // console.log("hi6");
 
         display.appendChild(bDiv);
 
@@ -95,10 +111,78 @@ function displayBooks() {
         bRemove.addEventListener('click', () => {
             display.removeChild(bDiv);
         })
-    }
+
+        // console.log("hi7");
+
+    
 }
 
 const book1 = new Book("Petunia","Laura",245,"Y");
-const book2 = new Book("Fireflies","Elliot",397,"N");
+// const book2 = new Book("Fireflies","Elliot",397,"N");
+// const book3 = new Book("Ecstacy","Medellin",601,"Y");
 
-displayBooks();
+
+
+
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.popUp')
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const title = document.getElementById('title')
+        const author = document.getElementById('author')
+        const pages = document.getElementById('pages')
+        const check = document.getElementById('read')
+
+        const a = title.value;
+        const b = author.value;
+        const c = pages.value;
+        const d = read.value;
+        
+        if(a != "" && b != "" && c!= "" && d != "")
+        {
+            const modal = button.closest('.popUp')
+            closeModal(modal)
+            const book = new Book(a,b,c,d);
+            // console.log("hi1");
+        }
+        else{
+            alert("Enter all fields");
+        }
+        
+        
+    })
+})
+
+function openModal(modal) {
+    if(modal == null)
+    return;
+
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if(modal == null)
+    return;
+
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
